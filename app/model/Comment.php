2865dda2cfbfddf140ab71ac439a31bde5409bc4
Ján Kovalčik <johny.kovalcik@gmail.com>
2016-03-03 10:13:14 +0100

    model comment corrected

--- app/model/Comment.php
+++ app/model/Comment.php
@@ -16,11 +16,17 @@ class Comment extends Object
      */
     private $created_at;
 
+    /**
+     * @var $content string
+     */
+    private $content
+
     public static function fromRow(IRow $row)
     {
         $comment = new self();
         $comment->commentId = $row->commentId;
         $comment->created_at = $row->created_at;
+        $comment->content = $row->content;
         return $comment;
     }
 }
