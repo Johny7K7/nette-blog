5184bbb0c8599741ca8c1a97fd552040000e1a1a
Radovan Kovalcik <radovankovalcik@gmail.com>
2016-03-17 22:10:50 +0100

    disable cache

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,8 +9,9 @@ $configurator->setDebugMode(FALSE);
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__ . '/../app')
+	->setCacheStorage(new \Nette\Caching\Storages\DevNullStorage())
 	->register();
 
 $configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
+$configurator->addConfig(__DIR__ . '/../app/config/config.test.neon');
 return $configurator->createContainer();
